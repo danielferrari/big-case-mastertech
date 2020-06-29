794cf6764d8fe0d07d6120af64c0608ac9daee34
Daniel Victor Ferrari <danielvictorferrari@gmail.com>
2020-06-29 16:02:03 -0300

    feat(nfe-calculo): ajuste em endpoint de efetivação de nfe

--- nfe-calculo/src/main/java/br/com/mastertech/nfecalculo/clients/NfeClient.java
+++ nfe-calculo/src/main/java/br/com/mastertech/nfecalculo/clients/NfeClient.java
@@ -2,7 +2,6 @@ package br.com.mastertech.nfecalculo.clients;
 
 import br.com.mastertech.nfecalculo.dtos.NfeCalculo;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "nfe")
 public interface NfeClient {
 
-    @PutMapping("/atualizar/{id}")
+    @PutMapping("/efetivar/{id}")
     void updateById(@PathVariable Long id, @RequestBody NfeCalculo nfeCalculo);
 }
